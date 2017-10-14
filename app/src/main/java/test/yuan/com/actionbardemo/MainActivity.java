2d74e2308a3b125e1f461de8e244e5c762648087
兰坡阳 <lanpoyang@meizu.com>
2017-10-14 10:33:58 +0800

    [ToolBar]将ToolBar设置为Activity的应用栏目； 问题原因： 解决方案： 验证建议：

--- app/src/main/java/test/yuan/com/actionbardemo/MainActivity.java
+++ app/src/main/java/test/yuan/com/actionbardemo/MainActivity.java
@@ -2,12 +2,22 @@ package test.yuan.com.actionbardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+/**
+ * @author lanpoyang
+ * ToolBar
+ * 使用ToolBar条件：（１）Android2.1(API级别７)或者更高版本；
+ * 使用：（１）向项目添加v7 appcompat支持库，（２）确保Activity可以扩展AppCompatActivity.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
