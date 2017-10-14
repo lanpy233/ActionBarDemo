94d951e77334a00787a0b59eb6fb2b66b17899de
兰坡阳 <lanpoyang@meizu.com>
2017-10-14 12:23:23 +0800

    [搜索导航栏]搜索按钮点击动作的布局文件； 问题原因：描述； 解决方案： 验证建议：

--- app/src/main/java/test/yuan/com/actionbardemo/SearchActivity.java
+++ app/src/main/java/test/yuan/com/actionbardemo/SearchActivity.java
@@ -24,31 +24,31 @@ public class SearchActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_menu, menu);
 
-        //定义一个监听器
-        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                // 当item折叠起来的时候的处理动作
-                Toast.makeText(SearchActivity.this, "当item折叠起来的时候的处理动作", Toast.LENGTH_SHORT).show();
-                return true;  // 返回true来折叠起这个动作视图
-            }
-
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                // 当item展开来的时候的处理动作
-                Toast.makeText(SearchActivity.this, "当item展开来的时候的处理动作", Toast.LENGTH_SHORT).show();
-                return true;  // 返回true来展开这个动作视图
-            }
-        };
-
-        // Get the MenuItem for the action item
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-
-        // 设置搜索内容并添加事件监听等等。。。
-        // 给这个动作视图添加监听
-        MenuItemCompat.setOnActionExpandListener(searchItem, expandListener);
-
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+//        //定义一个监听器
+//        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                // 当item折叠起来的时候的处理动作
+//                Toast.makeText(SearchActivity.this, "当item折叠起来的时候的处理动作", Toast.LENGTH_SHORT).show();
+//                return true;  // 返回true来折叠起这个动作视图
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                // 当item展开来的时候的处理动作
+//                Toast.makeText(SearchActivity.this, "当item展开来的时候的处理动作", Toast.LENGTH_SHORT).show();
+//                return true;  // 返回true来展开这个动作视图
+//            }
+//        };
+//
+//        // Get the MenuItem for the action item
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//
+//        // 设置搜索内容并添加事件监听等等。。。
+//        // 给这个动作视图添加监听
+//        MenuItemCompat.setOnActionExpandListener(searchItem, expandListener);
+//
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
         return super.onCreateOptionsMenu(menu);
     }
