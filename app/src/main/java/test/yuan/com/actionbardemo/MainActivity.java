97b529a3fdf90db126ab78bbfab4cd3434bc759b
兰坡阳 <lanpoyang@meizu.com>
2017-10-14 10:38:34 +0800

    [ToolBar] 设置ToolBar应用栏不显示； 问题原因： 解决方案： 验证建议：

--- app/src/main/java/test/yuan/com/actionbardemo/MainActivity.java
+++ app/src/main/java/test/yuan/com/actionbardemo/MainActivity.java
@@ -19,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        getSupportActionBar().hide();
     }
 }
